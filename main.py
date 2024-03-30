e59ea8f6251e58e50577d783d5afbe25cbff6d08
wsrn829 <wsrn829@gmail.com>
2024-03-30 00:15:48 -0400

    Fix cors

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 # CORS Middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://thrivetogether.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
